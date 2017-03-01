c2ee2c7a2f95b80b4f317f35986b9a66a9d03244
Peter Rud Frankild <Peter Rud Frankild>
2017-03-01 18:20:42 +0100

    add filter mangler sortering

--- petrograd_1.js
+++ petrograd_1.js
@@ -1,16 +1,12 @@
 window.addEventListener("load", sidenVises);
 
 function sidenVises() {
-    console.log("Siden vises");
-
-    // læs produktliste
+    console.log("siden vises");
+    //læs produktliste
     $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktListe);
-
+    //    visProdukt();
 }
-
-//vælger alle elementer med klassen "filterknap_vegetar". Der er tilføjet en eventlistener, der lytter til hver gang, der bliver udført et klik, så den vil eksekvere funktionen "filtrerVegetar"
-document.querySelector(".filterknap_vegetar").addEventListener("click", filtrerVegetar);
-
+document.querySelector(".filterKnap_vegetar").addEventListener("click", filtrerVegetar);
 document.querySelector(".filterknap_tilbud").addEventListener("click", filterTilbud);
 
 document.querySelector(".filterknap_alkohol").addEventListener("click", filtrerAlkohol);
@@ -20,62 +16,70 @@ document.querySelector(".sorter_billigste").addEventListener("click", sorterBill
 
 document.querySelector(".sorter_dyreste").addEventListener("click", sorterDyreste);
 
-// FILTRERINGSFUNKTIONER
-
 function filtrerVegetar(event) {
-    console.log("Klik på vegetar-filter");
-    // find alle ikke vegetar produkter
+    console.log("klik på vegetar-filter");
+    //find alle
     var liste = document.querySelectorAll(".produkt:not(.vegetar)");
-    // skjul dem - tilføj klassen "hide"
-    liste.forEach(produkt => produkt.classList.toggle("hide"));
+    //skjul all
+    liste.forEach(produkt => div.classList.toggle("hide"));
     event.preventDefault();
 }
 
 function filterTilbud(event) {
-    console.log("Klik på tilbud-filter");
-    // find alle produkter, der ikke er på tilbud
+    console.log("klik på tilbud-filter");
+    //find alle
     var liste = document.querySelectorAll(".produkt:not(.tilbud)");
-    liste.forEach(produkt => produkt.classList.toggle("hide"));
+    //skjul all
+    liste.forEach(produkt => div.classList.toggle("hide"));
     event.preventDefault();
 }
 
 function filtrerAlkohol(event) {
-    console.log("Klik på alkohol-filter");
+    console.log("klik på alkohol-filter");
+    //find alle
     var liste = document.querySelectorAll(".produkt:not(.alkohol)");
-    liste.forEach(produkt => produkt.classList.toggle("hide"));
+    //skjul all
+    liste.forEach(produkt => div.classList.toggle("hide"));
     event.preventDefault();
 }
 
-
-// SORTERINGSFUNKTION
-
-// billigst -> dyreste
 function sorterBilligste(listen) {
 
     console.log("Klik på sorter efter billigste -> dyreste");
 
 
 }
+//function visProduktListe ( listen ) {
+//    console.log(listen);
+//
+//    // filtrer udsolgte produkter fra...
+//    // For hvert produkt sender den produktet ind i beregningen og returnerer
+//    listen = listen.filter ( produkt => !produkt.udsolgt );
+//
+//    listen.forEach(visProdukt);
+//}
 
-
-// VIS NORMAL
 function visProduktListe(listen) {
-    console.log(listen);
+    //console.table(Listen);
+    //  filtrer udsolgte produkter fra
+    listen = listen.filter(fjernUdsolgte);
+    listen.forEach(visProdukt);
 
-    // filtrer udsolgte produkter fra...
-    // For hvert produkt sender den produktet ind i beregningen og returnerer
+    function fjernUdsolgte(produkt) {
+        if (produkt.udsolgt == true) {
+            return false;
+        } else {
+            return true;
+
+        }
+    }
     listen = listen.filter(produkt => !produkt.udsolgt);
-
     listen.forEach(visProdukt);
 }
 
 function visProdukt(produkt) {
     console.log(produkt);
-
-    //Vis produkt_template
     var klon = document.querySelector("#produkt_template").content.cloneNode(true);
-
-    // TILFØJER EN KLASSE, HVIS DEN ER VEGETAR ELLER ANDET
     if (produkt.vegetar) {
         klon.querySelector(".produkt").classList.add("vegetar");
     }
@@ -85,34 +89,28 @@ function visProdukt(produkt) {
     if (produkt.alkoholprocent) {
         klon.querySelector(".produkt").classList.add("alkohol");
     }
-
-    // indsæt data i klon
-    klon.querySelector(".data_navn").innerHTML = produkt.navn;
     klon.querySelector(".data_kortbeskrivelse").innerHTML = produkt.kortbeskrivelse;
+    klon.querySelector(".data_navn").innerHTML = produkt.navn;
     klon.querySelector(".data_pris").innerHTML = produkt.pris;
 
     var rabatpris = Math.ceil(produkt.pris - (produkt.pris * produkt.rabatsats / 100));
-
     klon.querySelector(".data_rabatpris").innerHTML = rabatpris;
 
-    klon.querySelector(".data_billede").src = "imgs/small/" + produkt.billede + "-sm.jpg";
+    klon.querySelector(".date_billede").src = "imgs/small/" + produkt.billede + "-sm.jpg";
 
     if (produkt.udsolgt == false) {
         var udsolgttekst = klon.querySelector(".udsolgttekst");
         udsolgttekst.parentNode.removeChild(udsolgttekst);
-
     } else {
         klon.querySelector(".pris").classList.add("udsolgt");
-    }
 
-    // hvis der er rabat, skal klassen "rabat" tilføjes
-    if (produkt.rabatsats == 0) {
+    }
+    if (produkt.udsolgt == true || produkt.rabatsats == 0) {
         var rabatpris = klon.querySelector(".rabatpris");
         rabatpris.parentNode.removeChild(rabatpris);
     } else {
         klon.querySelector(".pris").classList.add("rabat");
     }
-
     // tilføj produkt-id til modalknap
     klon.querySelector(".modalknap").dataset.produkt = produkt.id;
 
@@ -133,6 +131,9 @@ function visProdukt(produkt) {
 
 }
 
+document.querySelector(".produktliste").appendChild(klon);
+
+}
 //MODAL
 function modalKnapKlik(event) {
     console.log("knapklik", event);
@@ -173,7 +174,7 @@ function visModalProdukt(produkt) {
     klon.querySelector(".data_rabatpris").innerHTML = Math.ceil(produkt.pris - (produkt.pris * produkt.rabatsats / 100));
 
     // BILLEDE
-    klon.querySelector(".data_billede").src = "imgs/medium/" + produkt.billede + "-md.jpg";
+    klon.querySelector(".data_billede").src = "images/medium/" + produkt.billede + "-md.jpg";
 
     // LANG BESKRIVELSE
     klon.querySelector(".data_langbeskrivelse").innerHTML = produkt.langbeskrivelse;
