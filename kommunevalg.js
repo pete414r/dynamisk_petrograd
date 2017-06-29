82fa665110f0c020811918b3756e8b14e767c05f
Peter Rud Frankild <Peter Rud Frankild>
2017-06-29 13:46:13 +0200

    Setup and css

--- kommunevalg.js
+++ kommunevalg.js
@@ -94,3 +94,111 @@
 			document.querySelector("#person_info").appendChild(klon);
 		}
 
+	var map;
+
+window.addEventListener("load", initMap);
+
+function initMap() {
+	var center = {
+		lat: 55.689056,
+		lng: 12.597405
+	};
+	map = new google.maps.Map(document.getElementById("map"), {
+		zoom: 6,
+		center: center
+	});
+	$.getJSON("json/ture_maps_info.json", visKort);
+	var mig = new google.maps.Marker({
+		map: map,
+		icon: "images_turen/pointer_5turen.png",
+		title: "Husk at stemme"
+	});
+	if (navigator.geolocation) {
+		navigator.geolocation.watchPosition(function (position) {
+			console.log("SE DIN POSITION");
+			var minPos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+			map.setCenter(minPos);
+			mig.setPosition(minPos);
+		});
+	} else {
+		alert("Geolocation NOT");
+	}
+	function visKort(listen)  {
+		console.log(listen);
+		listen.forEach(visPunktInfo);
+	}
+	var bounds = {
+		north: 58.117764258222024,
+		south: 54.373327731522416,
+		east: 13.929399559375042,
+		west: 7.183794090625042
+	}
+	var overlay = new google.maps.GroundOverlay("images_turen/kort_kbhture_A12-01.svg", bounds);
+	overlay.setMap(map);
+	function visPunktInfo(punkt) {
+		console.log("Vis info");
+		console.log("id: " + punkt.id);
+		console.log("lat: " + punkt.lat);
+		console.log("lng: " + punkt.lng);
+		var ll = new google.maps.LatLng(punkt.lat, punkt.lng);
+		var m = new google.maps.Marker({
+			position: ll,
+			map: map,
+			icon: "images_turen/pointer_5turen.png",
+			title: "Husk at stemme til kommunalvalget",
+			animation: google.maps.Animation.DROP,
+			icon: punkt.icon
+		});
+		var infowindow = new google.maps.InfoWindow({
+			maxWidth: 500
+		});
+		m.addListener("click", visinfo);
+		function visinfo() {
+			var tempinfo =
+				document.querySelector("#kort").content.cloneNode(true);
+			tempinfo.querySelector(".h2class").innerHTML = punkt.id;
+			tempinfo.querySelector(".re-responsive").src = punkt.video;
+			tempinfo.querySelector(".r-responsive").src = punkt.images;
+			tempinfo.querySelector(".res-responsive").src = punkt.images1;
+			tempinfo.querySelector(".h1class").innerHTML = punkt.txt;
+			tempinfo.querySelector(".pclass").innerHTML = punkt.title;
+			infowindow.setContent(tempinfo);
+			infowindow.open(map, m);
+		}
+	}
+	}
+		var valgte;
+		var info;
+		var aktiv;
+		$('#valgturen').load("images_sitet/cs_infografik_12345.svg", kortHentet);
+		function kortHentet() {
+			$.getJSON("json/infografik_maps_info.json", dataHentet);
+		};
+		function dataHentet(data) {
+			info = data;
+			$("#valgturen svg g g").on("mouseenter", onEnter);
+			$("#valgturen svg g g").on("mouseleave", onOut);
+		}
+		function onEnter(e) {
+			aktiv = $(e.currentTarget).children();
+			valgte = $(e.currentTarget).closest("g").attr("id");
+			console.log(valgte);
+			info.forEach(visTekst);
+		};
+		function onOut() {
+		}
+		function visTekst(val) {
+			if (val.id == valgte) {
+				document.querySelector("#section3 h4").textContent = val.sted;
+				document.querySelector("#section3 h5").textContent = val.beskrivelse;
+			}
+		};
+		$(document).ready(function () {
+			console.log("start");
+			$(".lukkeklik").click(function () {
+				var navbar_toggle = $('.navbar-toggle');
+				if (navbar_toggle.is(':visible')) {
+				navbar_toggle.trigger('click');
+				}
+			});
+		});
